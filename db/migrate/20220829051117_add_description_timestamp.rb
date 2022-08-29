0089165c5604bb467e0a1a73addb2af207005f34
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-28 22:14:14 -0700

    add article description, created_at, updated_at

---
+++ db/migrate/20220829051117_add_description_timestamp.rb
@@ -0,0 +1,7 @@
+class AddDescriptionTimestamp < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+    add_column :articles, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_044728) do
+ActiveRecord::Schema.define(version: 2022_08_29_051117) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text "description"
   end
 
 end
