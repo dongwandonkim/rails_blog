05ebc757a2ac0abf51a810b9b82570bab39397de
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-30 22:28:09 -0700

    add create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # @article = Article.new(params[:article])
+    # render plain: @article.inspect
+    @article.save
+
+    redirect_to article_path(@article)
+    # or
+    # redirect_to @article
   end
 end
